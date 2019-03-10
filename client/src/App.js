2b597a94255f0acffa4f08b02087b42f4c75ad82
devGeonil <jgi92@naver.com>
2019-03-10 23:43:28 +0900

    Customer API made

--- client/src/App.js
+++ client/src/App.js
@@ -22,34 +22,23 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    "id":1,
-    "image":"https://placeimg.com/64/64/1",
-    "name" : "geonil",
-    "birthday" : "920105",
-    "gender" : "mail",
-    "job" : "dev"
-  },
-  {
-    "id":2,
-    "image":"https://placeimg.com/64/64/2",
-    "name" : "gildong",
-    "birthday" : "920512",
-    "gender" : "femail",
-    "job" : "dev1"
-  },
-  {
-    "id":3,
-    "image":"https://placeimg.com/64/64/3",
-    "name" : "yoonha",
-    "birthday" : "9244",
-    "gender" : "femail",
-    "job" : "dev2"
-  }
-]
 
 class App extends Component {
+
+  state = {
+    customers:""
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(error => console.log(error))
+  }
+
+  callApi = async () => {
+    return await fetch("/api/customers").then(res => res.json())
+  }
+
   render() {
     const {classes} = this.props;
     return (
@@ -67,10 +56,12 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map((customer, index) => {
+              this.state.customers ?
+              this.state.customers.map((customer, index) => {
               return <Customer customer={customer} key={customer.id}/>
               })
-            }
+            : ""
+          }
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/api/hello", (req, res)=>{
-  res.send({message: "Hello, Geonil"});
+app.get("/api/customers", (req, res)=>{
+  res.send([
+      {
+        "id":1,
+        "image":"https://placeimg.com/64/64/1",
+        "name" : "geonil",
+        "birthday" : "920105",
+        "gender" : "mail",
+        "job" : "dev"
+      },
+      {
+        "id":2,
+        "image":"https://placeimg.com/64/64/2",
+        "name" : "gildong",
+        "birthday" : "920512",
+        "gender" : "femail",
+        "job" : "dev1"
+      },
+      {
+        "id":3,
+        "image":"https://placeimg.com/64/64/3",
+        "name" : "yoonha",
+        "birthday" : "9244",
+        "gender" : "femail",
+        "job" : "dev2"
+      }
+  ]);
 })
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`))
