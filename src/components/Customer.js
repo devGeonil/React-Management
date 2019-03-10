d50ed15b8e033f64f0b15efffed934c4846f077a
devGeonil <jgi92@naver.com>
2019-03-10 22:59:11 +0900

    style table

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,40 +1,22 @@
 import React, { Component } from 'react';
 
+//material-ui
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+
 class Customer extends Component{
   render(){
     return(
-      <div>
-        <CustomerProfile id={this.props.customer.id} name={this.props.customer.name} image={this.props.customer.image}/>
-        <CustomerInfo birthday={this.props.customer.birthday} gender={this.props.customer.gender} job={this.props.customer.job}/>
-      </div>
+      <TableRow>
+        <TableCell>{this.props.customer.id}</TableCell>
+        <TableCell><img src={this.props.customer.image} alt="profile"/></TableCell>
+        <TableCell>{this.props.customer.name}</TableCell>
+        <TableCell>{this.props.customer.birthday}</TableCell>
+        <TableCell>{this.props.customer.gender}</TableCell>
+        <TableCell>{this.props.customer.job}</TableCell>
+      </TableRow>
     )
   }
 }
-
-
-class CustomerProfile extends Component{
-  render(){
-    return (
-      <div>
-        <img src={this.props.image} alt="profile"/>
-        <h2>{this.props.name} ({this.props.id})</h2>
-      </div>
-    )
-  }
-}
-
-class CustomerInfo extends Component{
-  render(){
-    return(
-      <div>
-        <p>{this.props.birthday}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
-      </div>
-    )
-  }
-}
-
-
 
 export default Customer
