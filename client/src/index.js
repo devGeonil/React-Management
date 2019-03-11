740650fd852f8fcd10ece56f9e8734f81f84cfb7
devGeonil <jgi92@naver.com>
2019-03-12 01:08:41 +0900

    Complete

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeprovider, createMuiTheme} from '@material-ui/core/styles'
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
